c286c62d9ebfc1093bea915a7c770a8aa72ca4fa
DevBarham <saheedibrahimdamilare@gmail.com>
2023-05-10 12:13:11 +0100

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = '/api';
+const url = 'http://localhost:5000/api';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
